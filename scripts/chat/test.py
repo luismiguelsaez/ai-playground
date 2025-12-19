766e0497d7bbf1947416564d59d246a33fa3079e
Luismi <luismiguelsaez83@gmail.com>
2025-12-19 22:25:40 +0100

    Added test script

---
+++ scripts/chat/test.py
@@ -0,0 +1,27 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from colorama import Fore
+
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B", device_map="cuda:0")
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+inputs = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device)
+
+print(f"{Fore.GREEN}Input IDs:\n{Fore.RED}{inputs['input_ids']}{Fore.RESET}")
+print(
+    f"{Fore.GREEN}Input IDs shape:\n{Fore.RED}{inputs['input_ids'].shape}{Fore.RESET}"
+)
+
+outputs = model.generate(**inputs, max_new_tokens=2048)
+
+print(f"{Fore.GREEN}Outputs:\n{Fore.RED}{outputs}{Fore.RESET}")
+print(
+    f"{Fore.GREEN}Outputs decoded:\n{Fore.RED}{tokenizer.decode(outputs[0][inputs['input_ids'].shape[-1] :])}{Fore.RESET}"
+)
