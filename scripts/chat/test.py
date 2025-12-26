c7a4a699fc3f522283d9f88ca1af5275c9bd2d33
Luismi <luismiguelsaez83@gmail.com>
2025-12-26 18:48:50 +0100

    Updated opencode config

--- scripts/chat/test.py
+++ scripts/chat/test.py
@@ -1,7 +1,8 @@
 from sys import argv
 
 from colorama import Fore
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+from threading import Thread
 
 MAX_NEW_TOKENS = 10000
 
@@ -9,6 +10,9 @@ checkpoints = ["LiquidAI/LFM2-2.6B-Exp", "Qwen/Qwen3-0.6B"]
 
 tokenizer = AutoTokenizer.from_pretrained(checkpoints[0])
 model = AutoModelForCausalLM.from_pretrained(checkpoints[0], device_map="cuda:3")
+
+streamer = TextIteratorStreamer(tokenizer=tokenizer, skip_prompt=True)
+
 messages = [
     {
         "role": "user",
@@ -18,34 +22,34 @@ messages = [
 inputs = tokenizer.apply_chat_template(
     messages,
     add_generation_prompt=True,
-    tokenize=True,
-    return_dict=True,
-    return_tensors="pt",
-).to(model.device)
+    tokenize=False,
+    return_dict=False,
+)
+
+print(f"{Fore.GREEN}Inputs:{Fore.RESET}\n{Fore.BLUE}{inputs}{Fore.RESET}")
+input_ids = tokenizer.encode(inputs, return_tensors="pt").to(model.device)
 
 print(f"{Fore.GREEN}Input IDs: {Fore.RESET}", end="")
-for t in inputs["input_ids"][0].tolist():
+for t in input_ids[0].tolist():
     print(f"{Fore.BLUE}{t}{Fore.RESET}", end=" ")
 print()
 
 outputs = model.generate(
-    **inputs,
+    input_ids,
     max_new_tokens=MAX_NEW_TOKENS,
-    temperature=0.7,
-    min_p=0.5,
     repetition_penalty=1.05,
 )
 
 print(f"{Fore.GREEN}Output IDs: {Fore.RESET}", end="")
-inputs_shape = inputs["input_ids"].shape[-1]
+inputs_len = len(input_ids[0])
 c = 0
 for t in outputs[0].tolist():
-    if c < inputs_shape:
+    if c < inputs_len:
         print(f"{Fore.BLUE}{t}{Fore.RESET}", end=" ")
         c += 1
     else:
         print(f"{Fore.RED}{t}{Fore.RESET}", end=" ")
 print()
 
-decoded_outputs = tokenizer.decode(outputs[0][inputs_shape:], skip_special_tokens=True)
+decoded_outputs = tokenizer.decode(outputs[0][inputs_len:], skip_special_tokens=True)
 print(f"{Fore.GREEN}Outputs decoded:\n{Fore.RED}{decoded_outputs}{Fore.RESET}")
