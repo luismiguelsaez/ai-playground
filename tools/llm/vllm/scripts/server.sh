70e3940ee80b3cb71a8c40f7c4d3f87eb2a7eb60
Luismi <luismiguelsaez83@gmail.com>
2026-01-30 09:46:01 +0100

    Optimized Minimax config

--- tools/llm/vllm/scripts/server.sh
+++ tools/llm/vllm/scripts/server.sh
@@ -6,14 +6,14 @@
 
 CHECKPOINT="LiquidAI/LFM2.5-1.2B-Instruct"
 
-vllm serve \
-  --model=$CHECKPOINT \
-  --kv-cache-dtype=fp8 \
-  --calculate-kv-scales \
-  --gpu-memory_utilization=0.25 \
-  --max-num_batched_tokens=8192 \
-  --max-num_seqs=256 \
-  --max-model_len=8192 \
-  --cpu-offload_gb=0 \
-  --tensor-parallel_size=1 \
-  --pipeline-parallel_size=1
+vllm serve --config ./config.yaml
+#  --model=$CHECKPOINT \
+#  --kv-cache-dtype=fp8 \
+#  --calculate-kv-scales \
+#  --gpu-memory_utilization=0.25 \
+#  --max-num_batched_tokens=8192 \
+#  --max-num_seqs=256 \
+#  --max-model_len=8192 \
+#  --cpu-offload_gb=0 \
+#  --tensor-parallel_size=1 \
+#  --pipeline-parallel_size=1
