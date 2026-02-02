978b766aa983661ad60ea3b0b20d1397e1b56e3d
Luismi <luismiguelsaez83@gmail.com>
2026-02-02 08:07:44 +0100

    Updated Minimax context

--- tools/llm/vllm/scripts/server.sh
+++ tools/llm/vllm/scripts/server.sh
@@ -10,6 +10,6 @@ CONFIG_FILE=${1:-lfm.yaml}
 VLLM_LOG_FILE=/tmp/vllm_serve.log
 OPENWEBUI_LOG_FILE=/tmp/openwebui.log
 
-vllm serve --config ${CONFIG_FILE}${CONFIG_FILE} 2>&1 >${VLLM_LOG_FILE} &
+vllm serve --config ${CONFIG_DIR}${CONFIG_FILE} 2>&1 >${VLLM_LOG_FILE} &
 
 OPENAI_API_BASE_URL=http://localhost:8000/v1 open-webui serve --host 0.0.0.0 --port 8888 2>&1 >${OPENWEBUI_LOG_FILE} &
