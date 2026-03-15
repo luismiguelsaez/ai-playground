042b9063f3cde4102f36bc3d20cceb27bf24878d
Luismi <luismiguelsaez83@gmail.com>
2026-03-15 11:10:27 +0100

    Added drivers install script

--- setup/nvidia-driver.sh
+++ setup/nvidia-driver.sh
@@ -7,6 +7,10 @@ sudo apt install -y nvidia-open-580
 sudo systemctl reboot
 nvidia-smi
 
+## Install proprietary Nvidia driver ( optional )
+
+- [Latest driver 580.142](https://us.download.nvidia.com/XFree86/Linux-x86_64/580.142/NVIDIA-Linux-x86_64-580.142.run)
+
 ## Disable DRM which makes the first GPU disappear
 echo "options nvidia_drm modeset=0" | sudo tee /etc/modprobe.d/nvidia-drm-nomodeset.conf
 sudo update-initramfs -u
