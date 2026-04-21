8a772d2600be26a6907a08552cc73c5f5e1852c8
Luismi <luismiguelsaez83@gmail.com>
2026-04-21 10:57:59 +0200

    Updated Minimax 2.7

--- tools/llm/vllm/source/build-source.sh
+++ tools/llm/vllm/source/build-source.sh
@@ -2,3 +2,6 @@
 
 MAKEFLAGS="-j$(nproc)" uv pip install -e vllm --torch-backend=auto --extra-index-url https://download.pytorch.org/whl/cu130 --index-strategy unsafe-best-match
 uv pip install transformers -U
+
+MAKEFLAGS="-j$(nproc)" uv pip install git+https://github.com/vllm-project/vllm@v0.19.2rc0 --extra-index-url https://download.pytorch.org/whl/cu130 --index-strategy unsafe-best-match
+uv pip install git+https://github.com/huggingface/transformers@v5.5.4
