eb76913183c7ad92e07ab3b5b6e6bcacd1793c14
Luismi <luismiguelsaez83@gmail.com>
2026-04-26 20:33:47 +0200

    Added test script

--- tools/llm/vllm/scripts/RTX3090/03-test.sh
+++ tools/llm/vllm/scripts/RTX3090/03-test.sh
@@ -1,3 +1,22 @@
 source .venv/bin/activate
 
+# 20.380Gi on startup
 vllm serve Lorbus/Qwen3.6-27B-int4-AutoRound --kv-cache-dtype fp8_e4m3 --max-model-len 30000 --max-num-seqs 1
+
+# Patches applied
+vllm serve Lorbus/Qwen3.6-27B-int4-AutoRound \
+  --host 0.0.0.0 --port 8000 \
+  --dtype float16 \
+  --tensor-parallel-size 1 \
+  --trust-remote-code \
+  --kv-cache-dtype turboquant_3bit_nc \
+  --max-num-seqs 1 \
+  --max-num-batched-tokens 4128 \
+  --max-model-len 125000 \
+  --reasoning-parser qwen3 \
+  --enable-auto-tool-choice \
+  --tool-call-parser qwen3_coder \
+  --enable-prefix-caching \
+  --enable-chunked-prefill \
+  --no-scheduler-reserve-full-isl \
+  --speculative-config '{"method":"mtp","num_speculative_tokens":3}'
