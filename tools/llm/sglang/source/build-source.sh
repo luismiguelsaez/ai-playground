7a7eab917dc763693c107c4852836f1e7724e27f
Luismi <luismiguelsaez83@gmail.com>
2026-04-27 19:21:36 +0200

    Added SGLang build script

---
+++ tools/llm/sglang/source/build-source.sh
@@ -0,0 +1,16 @@
+
+# Install
+
+- [Document](https://sgl-project.github.io/get_started/install.html#for-cuda-13)
+
+## Install PyTorch
+
+SGLANG_VERSION="0.5.10"
+
+uv pip install torch==$SGLANG_VERSION torchvision torchaudio --index-url https://download.pytorch.org/whl/cu130
+
+uv pip install sglang>=$SGLANG_VERSION
+
+uv pip install "https://github.com/sgl-project/whl/releases/download/v$SGLANG_VERSION/sglang_kernel-$SGLANG_VERSION+cu130-cp310-abi3-manylinux2014_x86_64.whl"
+
+MAKEFLAGS="-j$(nproc)" uv pip install git+https://github.com/sgl-project/sglang@v$SGLANG_VERSION --extra-index-url https://download.pytorch.org/whl/cu130 --index-strategy unsafe-best-match
