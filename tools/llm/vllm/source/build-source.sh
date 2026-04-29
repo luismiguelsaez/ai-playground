55cfde0f291ce506830dff9765f6729dcbda175c
Luismi <luismiguelsaez83@gmail.com>
2026-04-29 12:43:47 +0200

    Updated build.sh

--- tools/llm/vllm/source/build-source.sh
+++ tools/llm/vllm/source/build-source.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-MAKEFLAGS="-j$(nproc)" uv pip install -e vllm --torch-backend=auto --extra-index-url https://download.pytorch.org/whl/cu130 --index-strategy unsafe-best-match
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+uv venv --python 3.12
+
+VLLM_USE_PRECOMPILED=1 MAKEFLAGS="-j$(nproc)" uv pip install -e vllm --torch-backend=auto --extra-index-url https://download.pytorch.org/whl/cu130 --index-strategy unsafe-best-match
 uv pip install transformers -U
 
 MAKEFLAGS="-j$(nproc)" uv pip install git+https://github.com/vllm-project/vllm@v0.19.2rc0 --extra-index-url https://download.pytorch.org/whl/cu130 --index-strategy unsafe-best-match
