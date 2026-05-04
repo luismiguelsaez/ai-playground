3c20896bb7929e76e89f46da5a485a5bf7ffd690
Luismi <luismiguelsaez83@gmail.com>
2026-05-04 19:40:01 +0200

    Updated llama build

--- tools/llm/llamacpp/source/build.sh
+++ tools/llm/llamacpp/source/build.sh
@@ -1,26 +1,25 @@
 # Build
 
 ## Architectures
-
-- RTX 3090: `86`
-- RTX Pro 6000: `90`
+# - RTX 3090: `86`
+# - RTX Pro 6000: `90`
 
 ## Build command
+BUILD_DIR=${1:-"llama.cpp"}
 
-```bash
-CUDA_HOME=/usr/local/cuda-13 \
-CMAKE_PREFIX_PATH=/usr/local/cuda-13 \
-CUDA_ARCHS="90" \
-cmake -B build \
+cd $BUILD_DIR
+
+CUDA_HOME=${CUDA_HOME:-"/usr/local/cuda-13"} \
+  CMAKE_PREFIX_PATH=$CUDA_HOME \
+  CUDA_ARCHS=${CUDA_ARCHS:-"90"} \
+  cmake -B build \
   -DGGML_CUDA=ON \
   -DGGML_CUDA_FA=ON \
   -DGGML_CUDA_FA_ALL_QUANTS=ON \
-  -DCMAKE_CUDA_COMPILER=/usr/local/cuda-13/bin/nvcc \
-  -DCMAKE_CUDA_ARCHITECTURES="90" \
+  -DCMAKE_CUDA_COMPILER=$CUDA_HOME/bin/nvcc \
+  -DCMAKE_CUDA_ARCHITECTURES=$CUDA_ARCHS \
   -DLLAMA_OPENSSL=ON
 
 cmake --build build --config Release -j$(nproc) --clean-first
 
 sudo cp -rp build/bin/{llama-cli,llama-server,llama-bench,llama-quantize,llama-gguf-split} /usr/local/bin
-```
-
